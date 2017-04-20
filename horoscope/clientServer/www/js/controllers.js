e70ecefd2d519122f29c6d9712c29660ba91dd81
panpan0609 <726516795@qq.com>
2017-04-20 08:56:21 +0800

    clientServer

--- horoscope/clientServer/www/js/controllers.js
+++ horoscope/clientServer/www/js/controllers.js
@@ -1,26 +1,36 @@
-angular.module("starter.controllers",[])
-//分栏控制器
-.controller("mainController",function () {
+(function () {
+  angular.module("starter.controllers",[])
+  //分栏控制器
+    .controller("mainController",function () {
 
-})
-//星测试页面控制器
-.controller("starTestController",function ($scope) {
-  function MyCtrl($scope, $ionicSlideBoxDelegate) {
+    })
+    //星测试页面控制器
+    .controller("starTestController",function ($scope,$ionicSlideBoxDelegate) {
+      $scope.tabNames=['今天','明天','本周','本月','本年'];
+      $scope.slectIndex=0;
+      // $scope.activeSlide=function(index){//点击时候触发
+      //   $scope.slectIndex=index;
+      //   $ionicSlideBoxDelegate.slide(index);
+      // };
+      $scope.slideChanged=function(index){//滑动时候触发
+        $scope.slectIndex=index;
+      };
 
 
-  }
-})
+    })
 
-//星友圈页面控制器
-.controller("starFriendController",function () {
+    //星友圈页面控制器
+    .controller("starFriendController",function () {
 
-})
-//星愿墙页面控制器
-.controller("starWallController",function () {
+    })
+    //星愿墙页面控制器
+    .controller("starWallController",function () {
 
-})
-//星资料页面控制器
-.controller("starDataController",function () {
+    })
+    //星资料页面控制器
+    .controller("starDataController",function () {
 
-})
-;
+    })
+  ;
+
+})();
